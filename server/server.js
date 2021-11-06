69f1aa8453211b4bdd23ed1805d4e16063618dc8
JustinWeicht <justinweicht11@gmail.com>
2021-11-06 08:35:17 -0400

    server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,36 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import path, { resolve } from 'path';
-import dotenv from 'dotenv';
-import db from './config/connection.js';
-import { authMiddleware } from './utils/auth.js';
-import { typeDefs, resolvers } from './schemas/index.js';
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
-const app = express();
-dotenv.config();
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
+const app = express();
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware 
 });
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// The variable __dirname is not exposed by node when using ES6 module imports.
-// We can recreate __dirname with an empty path.resolve()
-const dirname = path.resolve();
-
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(resolve(dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(resolve(dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`Now listening on localhost: ${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
